24af38d283beb33522001dfbb7e7358db13ee413
M-b850 <m-b850@protonmail.com>
2021-04-24 14:01:47 +0430

    Add calendar_id text.

--- yara/google_calendar/create_event.py
+++ yara/google_calendar/create_event.py
@@ -1,13 +1,17 @@
 from cal_setup import get_calendar_service
 import datetime
+import os
 
+DIR = os.path.dirname(os.path.realpath(__file__))
+TEXT_DIR = f'{DIR}/calender_id.txt'
+name = open(TEXT_DIR).readline()
 
 def create(body):
     # creates new event
     service = get_calendar_service()
     # If have any specific calendar just replace calendar id with primary
     event_result = service.events().insert(
-        calendarId='primary', body=body
+        calendarId=f'name', body=body
     ).execute()
 
     print("created event")
@@ -23,7 +27,7 @@ def list_events_summary():
     now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTCtime
     print('Getting List of events')
     events_result = service.events().list(
-        calendarId='primary', timeMin=now,
+        calendarId=f'{name}', timeMin=now,
         maxResults=100, singleEvents=True,
         orderBy='startTime').execute()
     events = events_result.get('items', [])
